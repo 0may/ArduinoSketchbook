ccbf53bc4e9b1528ffd24c58804b3cea3bf1f0d7
Oliver Mayer <mayer@adbk-nuernberg.de>
2018-01-18 13:37:51 +0100

    license and description changed

--- AdbkSondol/AdbkSondol.ino
+++ AdbkSondol/AdbkSondol.ino
@@ -1,21 +1,38 @@
-/*  Example of playing a sampled sound,
-    using Mozzi sonification library.
-  
-    Demonstrates one-shot samples scheduled
-    with EventDelay.
-  
-    Circuit: Audio output on digital pin 9 on a Uno or similar, or
-    DAC/A14 on Teensy 3.1, or 
-    check the README or http://sensorium.github.com/Mozzi/
-  
-    Mozzi help/discussion/announcements:
-    https://groups.google.com/forum/#!forum/mozzi-users
-  
-    Tim Barrass 2012, CC by-nc-sa.
-*/
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2017 Oliver Mayer, Akademie der Bildenden Kuenste Nuernberg. 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*****************************************************************************  
+ *  "Sondol" device code 
+ *****************************************************************************/
+
 
 #include <MozziGuts.h>
-#include <Sample.h> // Sample template
+#include <Sample.h> 
 #include <samples/impulse/sine_impulse25.h>
 #include <EventDelay.h>
 #include <RollingAverage.h>
@@ -23,8 +40,6 @@
 
 #define CONTROL_RATE 64
 
-// use: Sample <table_size, update_rate> SampleName (wavetable)
-//Sample <UOU_NUM_CELLS, AUDIO_RATE> aSample(UOU_DATA);
 Sample <IMPULSE25_NUM_CELLS, AUDIO_RATE> aSample(IMPULSE25_DATA);
 
 RollingAverage <int, 16> avgA0; // average value of A0 over the last 16 reads
@@ -41,8 +56,8 @@ int tdelay = 1500;
 
 void setup(){
   startMozzi(CONTROL_RATE);
-  aSample.setFreq((float) IMPULSE25_SAMPLERATE / (float) IMPULSE25_NUM_CELLS * 3); // play at the speed it was recorded
-  kTriggerDelay.set(1500); // 1500 msec countdown, within resolution of CONTROL_RATE
+  aSample.setFreq((float) IMPULSE25_SAMPLERATE / (float) IMPULSE25_NUM_CELLS * 3); 
+  kTriggerDelay.set(1500); 
 }
 
 
